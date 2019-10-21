def15a44564b680f8f0484e4e13e4169cb175189
yan <1277218464@qq.com>
2019-10-21 22:44:55 +0800

    优化html中js异常问题1.0

--- springboot-websocket-learn/src/main/java/com/shengxi/entity/WiselyResponse.java
+++ springboot-websocket-learn/src/main/java/com/shengxi/entity/WiselyResponse.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import lombok.ToString;
 
 /**
  * @author yan
  * 推送类
  */
 @Data
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class WiselyResponse implements Serializable {
